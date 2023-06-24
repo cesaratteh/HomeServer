bf53918cc39e5dbea84405c71550c2b4c108fc0e
Cesar Atteh <cesar.atteh@gmail.com>
2023-06-23 21:15:15 -0700

    Configured jar creation, app is successfully running as a jar on linux Added Readme

---
+++ README.md
@@ -0,0 +1,16 @@
+# HomeService
+
+## To create JAR 
+Compile, then run assembly:single
+
+## Running the JAR
+java -jar <file>
+
+## System requirements
+
+* Google Chrome must be installed
+
+### Installing chrome for linux:
+
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo apt -y install ./google-chrome-stable_current_amd64.deb
--- src/main/java/com/eve/config/AppConfig.java
+++ src/main/java/com/eve/config/AppConfig.java
@@ -3,7 +3,7 @@ package com.eve.config;
 public class AppConfig {
     public static final String LOGGER_LOG_FILE = "HomeServerLog_";
 
-    public static final boolean SELENIUM_DRIVER_FACTORY_HEADLESS_MODE = false;
+    public static final boolean SELENIUM_DRIVER_FACTORY_HEADLESS_MODE = true;
     public static final int SYSTEM_FACTORY_EXECUTOR_THREAD_POOL_SIZE = 5;
 
     public final static String IFTTT_WEBHOOK_NOTIFIER_IFTTT_API_KEY = "bKQdb9G-C3bN2KUqU6S2cM";
